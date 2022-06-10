5fa0cfca3925290a6248ff8a910353519651118a
jinghe807 <87161057+jinghe807@users.noreply.github.com>
2022-06-09 23:33:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,13 @@
-/* sending request */
-let response = fetch("./src/education.json")
-
-fetch("./src/education.json")
-    .then(response => {
-        return response.json;
-    })
-    .catch(error => {
-        console.log(error);
-    });
-
-/* Resolve the promise using the Response class */
-
-fetch("./src/education.json")
-    .then(response => {
-       return response.json();
-    })
-    .then(jsondata => console.log(jsondata));
-
-/*async function fetchText() {
-    let response = await fetch("./src/education.json");
-    let data = await response.text();
-    console.log(data);
-} */
-
-/*Check the status code of the response*/
-async function fetchText() {
-    let response = await fetch("./src/education.json");
-
-    console.log(response.status); // 200
-    console.log(response.statusText); // OK
-
-    if (response.status === 200) {
-        let data = await response.text();
-        // handle data
-    }
+async function getDegrees(url) {
+  // fetch the url
+  await fetch(url)
+    //get your data here, and check for the response status. If it's not 200, throw an error
+    .then((response) => response.json())
+    .then((data) =>
+      document.write(
+        `My first degree was a ${data.data[0].collegedegrees.school}`
+      )
+    );
 }
 
-fetchText();
+getDegrees("./src/education.json");
