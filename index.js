97150322a6e6b02372fb5b67755a0b3e76fd2fdd
Jing He <jinghe@bu.edu>
2022-06-10 13:25:27 -0400

    update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ async function getDegrees(url) {
 	  await fetch(url)
 	    //get your data here, and check for the response status. If it's not 200, throw an error
 	    .then((response) => response.json())
-	    .then((data) =>
-	    for (var i=0;i<data.length;i++){
+	    .then((data) => 
+		{for (var i=0;i<data.length;i++){
 	      document.write(
 	        `My first degree was a ${data.data[0].degrees.Program_Major} ${data.data[0].degrees.Type} from ${data.data[0].degrees.School} in ${data.data[0].degrees.Year_conferred}`
 	      )}
-	    );
+		  });
 	}
 	
 
