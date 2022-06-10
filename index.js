f97baf53da7b62eb1a4b4b03d640a3dd816e4662
jinghe807 <87161057+jinghe807@users.noreply.github.com>
2022-06-10 13:00:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ async function getDegrees(url) {
 	    //get your data here, and check for the response status. If it's not 200, throw an error
 	    .then((response) => response.json())
 	    .then((data) =>
-	    for (i=0, i<data.length;i++){
+	    for (i=0;i<data.length;i++){
 	      document.write(
 	        `My first degree was a ${data.data[0].degrees.Program_Major} ${data.data[0].degrees.Type} from ${data.data[0].degrees.School} in ${data.data[0].degrees.Year_conferred}`
 	      )}
