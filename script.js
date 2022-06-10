04e2269f19875a78b638380a51434ffa91e6d9dc
jinghe807 <87161057+jinghe807@users.noreply.github.com>
2022-06-09 23:37:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ async function getDegrees(url) {
     .then((response) => response.json())
     .then((data) =>
       document.write(
-        `My first degree was a ${collegedegrees}`
+        `My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.institution} in ${data.data[0].degrees.bachelors.data.date}`
       )
     );
 }
 
-getDegrees("education.json");
+getDegrees("degrees.json");
